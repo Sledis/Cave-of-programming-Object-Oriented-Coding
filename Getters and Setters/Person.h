0c9501ec79de3688f7c89d3b5e9662e7075535bc
Sledis <49693807+Sledis@users.noreply.github.com>
2019-08-29 10:53:02 +0100

    Created a basic person class to implement basic getters and setters.

---
+++ Getters and Setters/Person.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<iostream>
+
+using namespace std;
+
+
+class Person
+{
+private:
+	string name;
+
+public:
+	Person();
+	string toString();
+	void setName(string newName);
+	string getName();
+};
+
